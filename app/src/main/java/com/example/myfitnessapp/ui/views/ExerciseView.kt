32063ea87c83ec1a2ad2ac7e0c0a40c1094efb90
Enzo <enzo.romagne@etu.univ-amu.fr>
2025-02-26 15:22:49 +0100

    Amélioration ExerciseView

--- app/src/main/java/com/example/myfitnessapp/ui/views/ExerciseView.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/views/ExerciseView.kt
@@ -1,5 +1,8 @@
 package com.example.myfitnessapp.ui.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,41 +12,70 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.example.myfitnessapp.models.datas.Exercise
-import com.example.myfitnessapp.models.datas.User
 import com.example.myfitnessapp.ui.theme.Modifiers
 import com.example.myfitnessapp.ui.theme.titleXSmall
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun ExerciseView(modifiers: Modifiers, exercise: Exercise, isSelected: Boolean, index: Int?, onSelectionChange: (Boolean) -> Unit) {
+fun ExerciseView(
+    modifiers: Modifiers,
+    exercise: Exercise,
+    isSelected: Boolean,
+    index: Int?,
+    onSelectionChange: (Boolean) -> Unit
+) {
+    val shapes = Shapes()
+
     Row(
-        modifier = modifiers.containerModifier,
+        modifier = modifiers
+            .containerModifier
+            .background(MaterialTheme.colorScheme.primary, shape = shapes.medium)
+            .clickable { onSelectionChange(!isSelected) }
+            .padding(modifiers.innerPadding),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
+        AsyncImage(
+            model = exercise.gifUrl,
+            contentDescription = "Image de l'exercice",
+            modifier = Modifier
+                .weight(0.3f)
+                .clip(shapes.medium),
+            contentScale = ContentScale.Crop
+        )
+
         Column(
-            modifier = Modifier.weight(0.7f)
+            modifier = Modifier
+                .weight(0.7f)
+                .padding(start = modifiers.bigPadding),
         ) {
             Text(
                 text = exercise.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() },
                 maxLines = 2,
-                style = MaterialTheme.typography.titleSmall)
-
-            Box(modifier = modifiers.containerModifier,
-                contentAlignment = Alignment.CenterStart){
-                FlowRow(
-                    modifier = Modifier.padding(modifiers.innerPadding),
+                style = MaterialTheme.typography.titleSmall
+            )
+            Box(
+                modifier = modifiers.containerModifier,
+                contentAlignment = Alignment.CenterStart
+            ){
+                Row(
+                    modifier = Modifier
+                        .horizontalScroll(rememberScrollState())
                 ) {
                     exercise.secondaryMuscles.forEach { muscle ->
                         SecondaryMuscleTagView(modifiers, muscle)
@@ -57,15 +89,25 @@ fun ExerciseView(modifiers: Modifiers, exercise: Exercise, isSelected: Boolean, 
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.End,
             modifier = Modifier
-                .weight(0.3f)
+                .weight(0.2f)
                 .fillMaxWidth()
         ) {
             if (isSelected) {
-                Text(text = index.toString(), style = MaterialTheme.typography.titleXSmall, modifier = Modifier.padding(modifiers.innerPadding) )
+                Text(
+                    text = index.toString(),
+                    style = MaterialTheme.typography.titleXSmall,
+                    modifier = Modifier.padding(modifiers.innerPadding),
+                    color = Color.White
+                )
             }
             Checkbox(
                 checked = isSelected,
-                onCheckedChange = { onSelectionChange(it) }
+                onCheckedChange = { onSelectionChange(it) },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color.White,
+                    uncheckedColor = Color.White,
+                    checkmarkColor = MaterialTheme.colorScheme.primary
+                )
             )
         }
     }
@@ -73,7 +115,7 @@ fun ExerciseView(modifiers: Modifiers, exercise: Exercise, isSelected: Boolean, 
 
 @Preview(showBackground = true)
 @Composable
-fun HomeScreenPreview() {
+fun ExerciseViewPreview() {
 
     val modifiers = Modifiers()
 
