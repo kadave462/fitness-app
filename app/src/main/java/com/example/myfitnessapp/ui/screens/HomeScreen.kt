512b56b262ac68ad8a6a5db1eba328e6906d69fc
david <davidmuhirekabera462@gmail.com>
2025-03-20 15:49:17 +0100

    dont

--- app/src/main/java/com/example/myfitnessapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screens/HomeScreen.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -17,37 +15,43 @@ import com.example.myfitnessapp.models.entities.User
 import com.example.myfitnessapp.ui.components.FloatingButtonView
 import com.example.myfitnessapp.ui.theme.Modifiers
 import com.example.myfitnessapp.viewmodels.utils.TimeUtils
+import java.time.LocalDate
+import java.util.Date
+
 
 @Composable
 fun HomeScreen(modifiers: Modifiers, navController: NavController, user: User) {
     Column(modifier = modifiers.bigPaddingModifier(false)) {
 
-        Spacer(modifier = Modifier.fillMaxHeight(0.05f))
+        Spacer(modifier = Modifier.height(modifiers.getScreenHeight() / 5f))
 
         Box(Modifier.weight(1f)
         ){
-            Column(
-                modifier = modifiers.containerModifier,
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Top
+
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.fitness_illu),
-                    contentDescription = "Illustration de l'image",
-                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
-                    modifier = Modifier.size((modifiers.getScreenWidth()) - modifiers.bigPadding)
+                    painter = painterResource(id = R.drawable.fitness_illustration),
+                    contentDescription = "Fitness Image",
+                    modifier = Modifier.size((modifiers.getScreenWidth()/2) - modifiers.bigPadding)
                 )
 
                 Spacer(modifier = Modifier.width(modifiers.innerPadding))
-                
-                Text(
-                    text = "Salut ${user.pseudonym}, prêt(e) à sortir de ton canapé ?",
-                    style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center)
-                )
-                Text(
-                    text = "C'est le moment, il est ${TimeUtils().getCurrentTime()} !",
-                    style = MaterialTheme.typography.headlineSmall.copy(textAlign = TextAlign.Center)
-                )
+
+                Column(modifier = modifiers.containerModifier,
+                    verticalArrangement = Arrangement.Top) {
+
+                    Text(
+                        text = "Bonjour, ${user.pseudonym}",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Text(
+                        text = "Il est ${TimeUtils().getCurrentTime()}",
+                        style = MaterialTheme.typographclcy.titleSmall
+                    )
+                }
             }
         }
 
@@ -58,13 +62,17 @@ fun HomeScreen(modifiers: Modifiers, navController: NavController, user: User) {
                 navController.navigate("exercise_screen")
             }
         }
+
+
     }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    val user = User(10, "AlexL", "", "Alex", "Laffite", "alex.laffite@gmail.com", 80.0, 180, "1995-06-15", "Homme", "Débutant")
+    val user = User(10, "AlexL", "Alex", "Laffite", "alex.laffite@gmail.com", 80.0, 180, "1995-06-15", "Homme", "Débutant")
     val modifiers = Modifiers()
     HomeScreen(modifiers, navController = rememberNavController(), user)
 }
