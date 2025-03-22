a5f7022982fa83f7bb8d138f6361c30d81015f90
david <davidmuhirekabera462@gmail.com>
2025-03-22 06:04:34 +0100

    made the id dynamique

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,13 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
+    //Authentication with Credential Manager
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
+
     implementation(libs.jbcrypt)
 
     ksp("androidx.room:room-compiler:2.6.1")
