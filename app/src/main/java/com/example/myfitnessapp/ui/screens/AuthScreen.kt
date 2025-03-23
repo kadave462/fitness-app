c27317bca256ac7d966512b49552cccabf434e60
Enzo <enzo.romagne@etu.univ-amu.fr>
2025-03-23 22:42:51 +0100

    Amélioration UI AuthScreen et HomeScreen

--- app/src/main/java/com/example/myfitnessapp/ui/screens/AuthScreen.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screens/AuthScreen.kt
@@ -1,11 +1,13 @@
 package com.example.myfitnessapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -13,19 +15,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.myfitnessapp.R
 
 @Composable
 fun AuthScreen(navController: NavController) {
     Column(
         modifier = Modifier.fillMaxSize().padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
-        Text(text = "Bienvenue", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.fillMaxHeight(0.05f))
 
-        Spacer(modifier = Modifier.fillMaxHeight(0.02f))
+        Image(
+            painter = painterResource(id = R.drawable.fitness_illu),
+            contentDescription = "Illustration de l'application",
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+            modifier = Modifier
+                .height(300.dp)
+        )
+
+        Text(
+            text = "L'appli qui te fera sortir de ton canapé !",
+            style = MaterialTheme.typography.headlineMedium.copy(textAlign = TextAlign.Center)
+        )
+
+        Spacer(modifier = Modifier.fillMaxHeight(0.05f))
 
         Button(
             onClick = { navController.navigate("login_screen") },
--- app/src/main/java/com/example/myfitnessapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/myfitnessapp/ui/screens/HomeScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -20,35 +22,32 @@ import com.example.myfitnessapp.viewmodels.utils.TimeUtils
 fun HomeScreen(modifiers: Modifiers, navController: NavController, user: User) {
     Column(modifier = modifiers.bigPaddingModifier(false)) {
 
-        Spacer(modifier = Modifier.height(modifiers.getScreenHeight() / 5f))
+        Spacer(modifier = Modifier.fillMaxHeight(0.05f))
 
         Box(Modifier.weight(1f)
         ){
-            Row(
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.Top
-
+            Column(
+                modifier = modifiers.containerModifier,
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.fitness_illustration),
-                    contentDescription = "Fitness Image",
-                    modifier = Modifier.size((modifiers.getScreenWidth()/2) - modifiers.bigPadding)
+                    painter = painterResource(id = R.drawable.fitness_illu),
+                    contentDescription = "Illustration de l'image",
+                    colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                    modifier = Modifier.size((modifiers.getScreenWidth()) - modifiers.bigPadding)
                 )
 
                 Spacer(modifier = Modifier.width(modifiers.innerPadding))
-
-                Column(modifier = modifiers.containerModifier,
-                    verticalArrangement = Arrangement.Top) {
-
-                    Text(
-                        text = "Bonjour, ${user.pseudonym}",
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                    Text(
-                        text = "Il est ${TimeUtils().getCurrentTime()}",
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                }
+                
+                Text(
+                    text = "Salut ${user.pseudonym}, prêt(e) à sortir de ton canapé ?",
+                    style = MaterialTheme.typography.headlineLarge.copy(textAlign = TextAlign.Center)
+                )
+                Text(
+                    text = "C'est le moment, il est ${TimeUtils().getCurrentTime()} !",
+                    style = MaterialTheme.typography.headlineSmall.copy(textAlign = TextAlign.Center)
+                )
             }
         }
 
